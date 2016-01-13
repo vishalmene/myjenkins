c35a94c56dc49318cb60e45483e9ee9f03774153
Vishal Mene <vishal.mene@gmail.com>
2016-01-13 14:55:37 +0530

    Revert Changes

--- src/MyJenkinsBundle/Controller/DefaultController.php
+++ src/MyJenkinsBundle/Controller/DefaultController.php
@@ -5,11 +5,13 @@ namespace MyJenkinsBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
-class DefaultController extends Controller {
+class DefaultController extends Controller
+{
     /**
      * @Route("/hello")
      */
-    public function indexAction() {
+    public function indexAction() 
+    {
     
         return $this->render('MyJenkinsBundle:Default:index.html.twig');
     }
