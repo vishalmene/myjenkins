d961ba176bdacdd5f65f41b86fb25ef970558325
Vishal Mene <vishal.mene@gmail.com>
2016-01-15 16:51:00 +0530

    remove Error

--- src/MyJenkinsBundle/Controller/DefaultController.php
+++ src/MyJenkinsBundle/Controller/DefaultController.php
@@ -13,6 +13,6 @@ class DefaultController extends Controller
     public function indexAction() 
     {
     
-       eturn $this->render('MyJenkinsBundle:Default:index.html.twig');
+       return $this->render('MyJenkinsBundle:Default:index.html.twig');
     }
 }
