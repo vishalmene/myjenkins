69dc41ad3d5bc4c6ea502d8de4c4e56c0815c6e4
Vishal Mene <vishal.mene@gmail.com>
2016-01-25 12:27:46 +0530

    Pass build

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -16,6 +16,6 @@ class DefaultController extends Controller
         // replace this example code with whatever you need
          return $this->render('default/index.html.twig', array(
             'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
-        ))
+        ));
     }
 }
