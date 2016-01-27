f137c5e9838cbb052b7d46bd92d2a7adc5bb7622
Vishal Mene <vishal.mene@gmail.com>
2016-01-27 14:11:36 +0530

    Modified for fail test

--- src/MyJenkinsBundle/Controller/DefaultController.php
+++ src/MyJenkinsBundle/Controller/DefaultController.php
@@ -13,6 +13,6 @@ class DefaultController extends Controller
     public function indexAction()
     {
     
-//                return $this->render('MyJenkinsBundle:Default:index.html.twig');
+                return $this->render('MyJenkinsBundle:Default:index.html.twig');
     }
 }
